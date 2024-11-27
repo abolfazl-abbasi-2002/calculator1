3ebad700160504480a927f8847d00e9b16d6e273
dev-abolfazlabbasi82 <engineerabolfazl82@gmail.com>
2024-11-27 19:06:02 +0330

    calculator codes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// helooo world
+// calculator
+const safe = document.getElementById("safe");
+
+function tosafe(input) {
+  safe.value += input;
+}
+function poww(input1, input2) {
+  safe.value = Number(Math.pow(input1, 2));
+}
+function cl() {
+  safe.value = " ";
+}
+
+function cuc() {
+  try {
+    safe.value = eval(safe.value);
+  } catch (error) {
+    safe.value = "Error";
+  }
+}
